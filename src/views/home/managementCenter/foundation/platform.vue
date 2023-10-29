708e06c7731ae092af1f91c74de86f5dff2198b1
ivory <jsivory@itcom666.com>
2023-10-29 16:53:07 +0800

    2023-10-29 16:52

---
+++ src/views/home/managementCenter/foundation/platform.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="bird2">
+        一个项目两个平台
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'platformUnit',
+    created() {
+        console.log("--root_platform--", root_platform);
+    }
+}
+</script>
+
+<style lang="scss">
+.bird2 {
+    @if $platform == 'bd' {
+        color: rosybrown;
+    }
+    @if $platform == 'tb' {
+        color: greenyellow;
+    }
+    @include platform-bd() {
+        background: rgb(77, 76, 76);
+    }
+    @include platform-tb() {
+        background: rgb(179, 134, 52);
+    }
+}
+</style>
